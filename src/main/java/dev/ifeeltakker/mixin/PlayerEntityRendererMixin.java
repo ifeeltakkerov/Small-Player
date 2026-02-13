ca64cadb64d33ab99a8bbd693f66ca0547307e65
ifeeltakker <denprokofev.12@gmail.com>
2026-02-13 15:10:11 +0300

    fabric 1.20.1

---
+++ src/main/java/dev/ifeeltakker/mixin/PlayerEntityRendererMixin.java
@@ -0,0 +1,26 @@
+package dev.ifeeltakker.mixin;
+
+import dev.ifeeltakker.PlayerSizeMod;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntityRenderer.class)
+public class PlayerEntityRendererMixin {
+
+    @Inject(
+            method = "render(Lnet/minecraft/client/network/AbstractClientPlayerEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            at = @At("HEAD")
+    )
+    private void preRender(AbstractClientPlayerEntity player, float yaw, float tickDelta,
+                           MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+
+        float scale = PlayerSizeMod.getPlayerSize(player.getUuid());
+        matrices.scale(scale, scale, scale);
+    }
+}
